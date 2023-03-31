197b024941b446fbc504bcc383f02c8f2fa9212a
she3r <piotr.kubacki@student.uj.edu.pl>
2023-03-31 14:39:39 +0200

    init 5

---
+++ fifth/dynamicVector.h
@@ -0,0 +1,16 @@
+//
+// Created by Piotr Kubacki on 29.03.2023.
+//
+
+#ifndef EFFECTIVEMODERNCPPCOURSE_DYNAMICVECTOR_H
+#define EFFECTIVEMODERNCPPCOURSE_DYNAMICVECTOR_H
+
+#include <cstddef>
+#include "staticVector.h"
+
+template<typename T>
+class Vector<T,0>{
+
+};
+
+#endif //EFFECTIVEMODERNCPPCOURSE_DYNAMICVECTOR_H
---
+++ fifth/staticVector.h
@@ -0,0 +1,92 @@
+
+#ifndef STATIC_VECTOR_H
+#define STATIC_VECTOR_H
+
+
+#include <memory>
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <cmath>
+#include <cstring>
+template <typename T, size_t N>
+class Vector;
+template <typename T, size_t N>
+Vector<T,N> operator+ (const  Vector<T,N> & u, const Vector<T,N> & v );
+template <typename T, size_t N>
+class Vector{
+    T data[N];
+public:
+    typedef T value_type;
+    typedef std::size_t  size_type;
+    typedef T* pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
+
+    Vector() {
+        memset(data,0,sizeof(data));
+    }
+    Vector(const Vector & v) {
+        size_type size_of_v = std::end(v.data)-std::begin(v.data);
+        for(int i=0;i<std::min(size_of_v, N);++i){
+            data[i] = v[i];
+        }
+    }
+    Vector &operator=(const Vector & m) {
+        if(this != m){
+            size_type size_of_v = std::end(m.data)-std::begin(m.data);
+            for(int i=0;i<std::min(size_of_v, N);++i){
+                data[i] = m[i];
+            }
+        }
+    }
+    Vector(const std::initializer_list<T> &list){
+        size_t i = 0;
+        for(auto item : list){
+            if(i < N)
+                data[i] = item;
+            else break;
+
+            i++;
+        }
+    }
+
+    friend Vector operator+ <>(const  Vector & u, const Vector & v );
+
+    [[nodiscard]] constexpr size_type size() const {
+        return N;
+    }
+
+    const_reference get(size_type index) const {
+        return data[index];
+    }
+
+    void set(size_type index, const_reference value) {
+        data[index] = value;
+    }
+
+    reference operator[](size_type index){
+        return data[index];
+    }
+    const_reference operator[](size_type index) const{
+        return data[index];
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Vector & v) {
+        for( auto elem: v.data ){
+            out << elem << " ";
+        }
+        return out;
+    }
+
+};
+template <typename T, size_t N>
+Vector<T,N> operator+ (const  Vector<T,N> & u, const Vector<T,N> & v ){
+    Vector<T,N> out{};
+    size_t size_of_out = std::min(std::end(u.data)-std::begin(u.data),std::end(v.data)-std::begin(v.data));
+    for(size_t i = 0; i<size_of_out; ++i){
+        out[i] = u[i] + v[i];
+    }
+    return out;
+}
+#endif //STATIC_VECTOR_H
