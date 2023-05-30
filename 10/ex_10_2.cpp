8e708543e32b91f06ad17bec851cb8e3ae8094ed
she3r <sch03eer9@gmail.com>
2023-05-30 19:38:11 +0200

    fix 10.2

--- 10/ex_10_2.cpp
+++ 10/ex_10_2.cpp
@@ -7,39 +7,47 @@ using namespace std;
 // Created by user on 24.05.23.
 //
 
-template<class C>
-        double median(C& collection){
-            typedef typename std::iterator_traits<typename C::iterator>::iterator_category category;
-            size_t n = collection.size() / 2;
-            if constexpr (std::is_same_v<category, std::random_access_iterator_tag>) {
-                cout << "random access iterator version" << endl;
 
-                nth_element(collection.begin(), collection.begin()+n, collection.end());
-                if(collection.size() % 2 != 0)
-                    return collection[n];
-                else{
-                    double a = collection[n];
-                    nth_element(collection.begin(), collection.begin()+(n-1), collection.end());
-                    return (a + collection[n-1]) / 2;
-                }
-            }
-            else{
-                cout << "no random access iterator version" << endl;
-                collection.sort();
-                if(collection.size() % 2 != 0) {
-                    auto it = collection.begin();
-                    std::advance(it, n);
-                    return *it;
-                }
-                else{
-                    auto it = collection.begin();
-                    std::advance(it, n-1);
-                    double a = *it;
-                    std::advance(it,1);
-                    return (a + *it) / 2;
-                }
-            }
+
+template<class C>
+double median_(C& collection, std::random_access_iterator_tag) {
+
+    size_t n = collection.size() / 2;
+    cout << "random access iterator version" << endl;
+
+    nth_element(collection.begin(), collection.begin() + n, collection.end());
+    if (collection.size() % 2 != 0)
+        return collection[n];
+    else {
+        double a = collection[n];
+        nth_element(collection.begin(), collection.begin() + (n - 1), collection.end());
+        return (a + collection[n - 1]) / 2;
+    }
+}
+template<class C>
+double median_(C& collection, std::forward_iterator_tag){
+        cout << "no random access iterator version" << endl;
+        size_t n = collection.size() / 2;
+        collection.sort();
+        if(collection.size() % 2 != 0) {
+            auto it = collection.begin();
+            std::advance(it, n);
+            return *it;
         }
+        else{
+            auto it = collection.begin();
+            std::advance(it, n-1);
+            double a = *it;
+            std::advance(it,1);
+            return (a + *it) / 2;
+        }
+
+}
+template<class Container>
+double median(Container& container){
+    typename std::iterator_traits<typename Container::iterator>::iterator_category category;
+    return median_(container, category);
+}
 
 
 int main(){
